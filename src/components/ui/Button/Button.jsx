adca1addb4fc3708aa2941c6e94e002ecebe2035
abhi-zero <abhi.0zero@gmail.com>
2025-10-01 16:00:44 +0530

    refactor: fix background issue and add onclick

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function Button({text, hoverBg, hoverText, font, fontType, textColor}) {
+export default function Button({text,bg, hoverBg, hoverText, font, fontType, textColor, onClick}) {
   return (
    
         <button
-        className={`bg-neutral-0 hover:${hoverBg && hoverBg} backdrop-blur-2xl px-5 py-2.5 rounded-full ${font ? font : 'font-Fraunces'} ${fontType ? fontType : 'font-medium'} hover:${hoverText ? hoverText : 'text-neutral-0'} transition-all duration-500 ease-in-out cursor-pointer ${textColor && textColor}`}
+        onClick={onClick && onClick}
+        className={`${bg ? bg: 'bg-neutral-0'}  hover:${hoverBg ? hoverBg : 'bg-neutral-0/30'} backdrop-blur-2xl px-5 py-2.5 rounded-full ${font ? font : 'font-Fraunces'} ${fontType ? fontType : 'font-medium'} hover:${hoverText ? hoverText : 'text-neutral-0'} transition-all duration-500 ease-in-out cursor-pointer ${textColor && textColor}`}
         >{text}</button>
    
   )
