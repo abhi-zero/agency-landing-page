9b6893d56fe52fffc1be8a848e06cd6bc5e6ce77
abhi-zero <abhi.0zero@gmail.com>
2025-10-04 22:03:24 +0530

    refactore: add maxwidttth to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import Button from './components/ui/Button/Button'
 import Navbar from './components/Navbar/Navbar'
-import { HeroSetion } from './sections'
+import { AboutSection, HeroSetion } from './sections'
 
 function App() {
   return (
-    <div className=''>
+    <div className='max-w-[1950px] overflow-x-hidden'>
       <Navbar />
       <HeroSetion />
+      <AboutSection />
     </div>
   )
 }
