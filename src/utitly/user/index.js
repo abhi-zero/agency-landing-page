da0ee7ed330fe6b10825a1d6dcf9a25f885a56d5
abhi-zero <abhi.0zero@gmail.com>
2025-10-05 20:24:38 +0530

    chore: add user's testimonial

---
+++ src/utitly/user/index.js
@@ -0,0 +1,29 @@
+import EmilyImage from "../../assets/images/people/image-emily.jpg";
+import ThomasImage from "../../assets/images/people/image-thomas.jpg";
+import JennieImage from "../../assets/images/people/image-jennie.jpg";
+
+const testimonials = [
+  {
+    message:
+      "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+    user: "Emily R.",
+    userRole: "Marketing Director",
+    image: EmilyImage,
+  },
+  {
+    message:
+      "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.",
+    user: "Thomas S.",
+    userRole: "Chief Operating Officer",
+    image: ThomasImage,
+  },
+  {
+    message:
+      "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+    user: "Jennie F.",
+    userRole: "Business Owner",
+    image: JennieImage,
+  },
+];
+
+export default testimonials;
