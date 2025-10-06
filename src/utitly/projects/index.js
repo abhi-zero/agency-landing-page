920e98464c0c45afb8a5aed0ff5735ab43495363
abhi-zero <abhi.0zero@gmail.com>
2025-10-06 12:47:13 +0530

    chore: create project content

---
+++ src/utitly/projects/index.js
@@ -0,0 +1,33 @@
+import milkBottlesDesktop from '../../assets/images/desktop/image-gallery-milkbottles.jpg'
+import milkBottlesMobile from '../../assets/images/mobile/image-gallery-milkbottles.jpg'
+import coneDesktop from '../../assets/images/desktop/image-gallery-cone.jpg'
+import coneMobile from '../../assets/images/mobile/image-gallery-cone.jpg'
+import orangeDesktop from '../../assets/images/desktop/image-gallery-orange.jpg'
+import orangeMobile from '../../assets/images/mobile/image-gallery-orange.jpg'
+import sugarcubesDesktop from '../../assets/images/desktop/image-gallery-sugarcubes.jpg'
+import sugarcubesMobile from '../../assets/images/mobile/image-gallery-sugar-cubes.jpg'
+
+const projects = [
+    {
+        projectLink : "project1",
+        destopProjectImg : milkBottlesDesktop,
+        mobileProjectImg : milkBottlesMobile
+    },
+     {
+        projectLink : "project2",
+        destopProjectImg : orangeDesktop,
+        mobileProjectImg : orangeMobile
+    },
+     {
+        projectLink : "project3",
+        destopProjectImg : coneDesktop,
+        mobileProjectImg : coneMobile
+    },
+     {
+        projectLink : "project4",
+        destopProjectImg : sugarcubesDesktop,
+        mobileProjectImg : sugarcubesMobile
+    }
+]
+
+export default projects;
