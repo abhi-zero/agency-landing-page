5c4d996ad5634741c8ea7c3457306727f6f38dd0
abhi-zero <abhi.0zero@gmail.com>
2025-10-06 13:32:04 +0530

    update: add footer

--- src/sections/index.js
+++ src/sections/index.js
@@ -2,12 +2,14 @@ import HeroSetion from "./HeroSection/HeroSetion";
 import AboutSection from "./AboutSection/AboutSection";
 import ServiceSection from "./ServiceSection/ServiceSection";
 import TestimonialSection from "./TestimonialSection/TestimonialSection";
-import ProjectSection from "./ProjectSection/ProjectSection"
+import ProjectSection from "./ProjectSection/ProjectSection";
+import Footer from "./FooterSection/Footer";
 
 export {
     HeroSetion,
     AboutSection,
     ServiceSection,
     TestimonialSection,
-    ProjectSection
+    ProjectSection,
+    Footer
 }
