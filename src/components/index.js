0d01c66ce92b56f4a8b39df3cd0d5668d78f517c
abhi-zero <abhi.0zero@gmail.com>
2025-10-06 12:45:27 +0530

    update : update import and export

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import AboutCard from "./About/AboutCard/AboutCard";
 import ServiceCard from "./Services/ServiceCard/ServiceCard";
 import TestimonialCard from "./Testimonials/TestimonialCard/TestimonialCard";
+import ProjectCard from "./Project/ProjectCard/ProjectCard";
 
 export {
     AboutCard,
     ServiceCard,
-    TestimonialCard
+    TestimonialCard,
+    ProjectCard
 }
--- src/sections/index.js
+++ src/sections/index.js
@@ -2,10 +2,12 @@ import HeroSetion from "./HeroSection/HeroSetion";
 import AboutSection from "./AboutSection/AboutSection";
 import ServiceSection from "./ServiceSection/ServiceSection";
 import TestimonialSection from "./TestimonialSection/TestimonialSection";
+import ProjectSection from "./ProjectSection/ProjectSection"
 
 export {
     HeroSetion,
     AboutSection,
     ServiceSection,
-    TestimonialSection
+    TestimonialSection,
+    ProjectSection
 }
