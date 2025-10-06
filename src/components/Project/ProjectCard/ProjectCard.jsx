35582acdc48207c19170cb2f4e1f70dc0f594660
abhi-zero <abhi.0zero@gmail.com>
2025-10-06 16:01:57 +0530

    fix: hover effect

--- src/components/Project/ProjectCard/ProjectCard.jsx
+++ src/components/Project/ProjectCard/ProjectCard.jsx
@@ -28,10 +28,10 @@ export default function ProjectCard({
           <source media="(min-width: 769px)" srcSet={desktopImage} />
           <img
             src={desktopImage}
-            className="w-full h-full object-center object-cover"
+            className="w-full h-full object-center object-cover group-hover:scale-125 transition-all duration-300 ease-in-out"
           />
         </picture>
-        <div className="top-[100%] group-hover:top-[0%] absolute flex justify-center items-center-safe bg-neutral-0/9 backdrop-blur-sm w-full h-full transition-all duration-300 ease-in-out">
+        <div className="top-[100%] group-hover:top-[0%] absolute flex justify-center items-center-safe bg-neutral-950/40 backdrop-blur-sm w-full h-full transition-all duration-300 ease-in-out">
           <AnchorLinkAsButton
             text={"Learn more"}
             link={link}
