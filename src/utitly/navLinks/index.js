213b94c3370e4c71302281afc2b1304d82793b48
abhi-zero <abhi.0zero@gmail.com>
2025-10-06 13:32:33 +0530

    chore: create array for links

---
+++ src/utitly/navLinks/index.js
@@ -0,0 +1,18 @@
+
+
+const navLinks = [
+    {
+        name : "About",
+        navLink: "#about"
+    },
+     {
+        name : "Service",
+        navLink: "#service"
+    },
+     {
+        name : "Project",
+        navLink: "#project"
+    }
+]
+
+export default navLinks
